9f7f4ef377290e4d10aa4f23e9ea3f8120d5be92
Fasola Olabode <bodman2019@gmail.com>
2022-06-26 15:31:57 +0100

    third commit

--- env/I4G015513QJJ/blog/views.py
+++ env/I4G015513QJJ/blog/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from .models import Post 
 
 class PostListview(CreateView):
     model = Post 
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = [
+         "__all__"
+    ]
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post 
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = [
+         "__all__"
+    ]
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = [
+         "__all__"
+    ]
+    template_name = 'home.html'
 
 # Create your views here.
