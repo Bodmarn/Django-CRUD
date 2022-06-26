a2018e1063299a5a24d84a715a857c37a4903bb1
Fasola Olabode <bodman2019@gmail.com>
2022-06-26 02:20:32 +0100

    second commit

--- env/I4G015513QJJ/blog/views.py
+++ env/I4G015513QJJ/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Post 
+
+class PostListview(CreateView):
+    model = Post 
 
 # Create your views here.
